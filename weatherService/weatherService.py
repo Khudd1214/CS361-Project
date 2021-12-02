7da4b4159dfea9e3fac138a584e99cb457fad381
Khudd1214 <77385940+Khudd1214@users.noreply.github.com>
2021-12-01 17:26:48 -0800

    Add files via upload

--- weatherService/weatherService.py
+++ weatherService/weatherService.py
@@ -2,8 +2,10 @@ import os
 
 homeDirectoryPath = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 weatherDict = {
-    "oregon" : "Sun! Yes! To the water - oh wait, nvmd. Just started raining again. HOW? There aren't even clouds in the sky. Where is the rain coming from?",
-    "california" : "The sounds of plants and animals dying as we go 6 months without rain... Ooh! Another earthquake.",
+    "oregon" : """Sun! Yes! To the water - oh wait, nvmd. Just started raining again. 
+                        HOW? There aren't even clouds in the sky. Where is the rain coming from?""",
+    "california" : """The sounds of plants and animals dying as we go 6 
+                        months without rain... Ooh! Another earthquake.""",
     "washington" : "Cloudy with a chance of bear attacks",
     "new york" : "Cloudy with a chance of meatballs",
     "nevada" : "REALLY dry",
@@ -12,13 +14,16 @@ weatherDict = {
     "alaska" : "The polar bears would say its warm, but...",
     "arkansas" : "The lizards are crying - it's too darn hot!",
     "arizona" : "I think I saw lucifer dancing in the grand canyon...",
-    "colorado" : "Nice and chill. If I lived in arizona, I would be running for the colorado hills.",
+    "colorado" : """Nice and chill. If I lived in arizona, 
+                    I would be running for the colorado hills.""",
     "connecticut" : "If you like lightning and thunderstorms... what is wrong with you?",
     "delaware" : "Dull and Dry - just like the state.",
-    "florida" : "Weather is the least of your problems - the thousands of drunk teenagers are a much bigger concern...",
+    "florida" : """Weather is the least of your problems - the thousands 
+                    of drunk teenagers are a much bigger concern...""",
     "georgia" : "At least it has water... Just be ready to dodge the sharks.",
     "idaho" : "Get ready for ice.",
-    "illinois" : "A pleasant afternoon with sunshine and a gentle breeze... oh, nevermind. Now there's another thunderstorm",
+    "illinois" : """A pleasant afternoon with sunshine and a gentle breeze... 
+                    oh, nevermind. Now there's another thunderstorm""",
     "indiana" : "Cold and snowy. Not much else to be said.",
     "iowa" : "Snow in the winter - and the sounds of people weeping in a humid summers.",
     "kansas" : "I would tell you... but let's be honest, its gonna change again in 5 minutes.",
@@ -53,18 +58,21 @@ weatherDict = {
     "wisoncsin" : "REALLY dry",
     "wyoming" : "REALLY dry",
 }
+def formatContent():
+    rawFile = open(os.path.join(homeDirectoryPath, 'weather_request.csv'))
+    contents = ""
+    for line in rawFile:
+        for characters in line:
+            if characters in [' ', '\n']:
+                continue
+            contents += characters.lower()
+    rawFile.close()
+    return contents
+
 def readSourceFile():
     deletePreviousResponseFile()
     if os.path.exists(homeDirectoryPath + "/weather_request.csv"):
-        contents = str()
-        rawFile = open(os.path.join(homeDirectoryPath, 'weather_request.csv'))
-
-        for line in rawFile:
-            for characters in line:
-                if characters in [' ', '\n']:
-                    continue
-                contents += characters.lower()
-        rawFile.close()
+        contents = formatContent()
 
         input_state = contents.split(",")
 
@@ -79,28 +87,25 @@ def readSourceFile():
         err = "Could not find request file"
         writeError(err)
         return None
+        
 def writeError(errorString):
     with open(os.path.join(homeDirectoryPath, 'error.csv'), 'w') as output:
         output.write(errorString)
-    output.close()
+    return
 def writeData(state):
     text = weatherDict[state]
     with open(os.path.join(homeDirectoryPath, 'weather_response.csv'), 'w') as output:
         output.write(state[0].upper() + state[1::].lower() + " , " + text)
-    output.close()
+    return
 def deleteSourceFile():
     if os.path.exists(homeDirectoryPath + "/weather_request.csv"):
         os.remove(homeDirectoryPath + "/weather_request.csv")
-    return  #This return statement is implied, but I am including for the sake of clarity
+    return
 def deletePreviousResponseFile():
     if os.path.exists(homeDirectoryPath + "/weather_response.csv"):
         os.remove(homeDirectoryPath + "/weather_response.csv")
-    return  #This return statement is implied, but I am including for the sake of clarity
+    return
 
-#read the request file
 state = readSourceFile()
-#write the data in weather_response.csv
-#if state is not None:
 writeData(state)
-#delete weather_request.csv
 deleteSourceFile()
